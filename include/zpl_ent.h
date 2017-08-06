7c9ef239d59a79d40e42b545d16ab6482f6c5ff5
Dominik Madarasz <madaraszd@mail.ru>
2017-08-06 20:12:26 +0200

    Image and Entity modules *WIP

--- include/zpl_ent.h
+++ include/zpl_ent.h
@@ -6,89 +6,86 @@ License:
     This software is dual-licensed to the public domain and under the following
     license: you are granted a perpetual, irrevocable license to copy, modify,
     publish, and distribute this file as you see fit.
-    
+
 Warranty:
     No warranty is implied, use at your own risk.
-    
+
 Usage:
-    #define ZPL_ENT_IMPLEMENTATION exactly in ONE source file right BEFORE including the library, like:
-    
-    #define ZPL_ENT_IMPLEMENTATION
+    #define ZPLE_IMPLEMENTATION exactly in ONE source file right BEFORE including the library, like:
+
+    #define ZPLE_IMPLEMENTATION
     #include"zpl_ent.h"
-    
+
 Optional Switches:
     ZPL_USE_128_BITS -- uses 128-bit storage for entity identification handle.
-    
+
 Important note:
     - Component pool of type X can be used ONLY BY ONE entity pool.
     - Entity pool can use MULTIPLE component pools of type X.
     - Entity handles SHALL NOT BE mixed-up with DIFFERENT entity pools / component pools.
     - Entity's components HAVE TO BE detached BEFORE the entity gets destroyed.
-    
-    
+
+
 Credits:
     Dominik Madarasz (GitHub: zaklaus)
     Vladislav Gritsenko (GitHub: inlife)
-    
+
 Version History:
-    1.06 - Added small example
-    1.05 - Got rid of redundant field
-    1.00 - Initial version
-    
+    1.0.0 -- Initial version
 */
 
 #ifndef ZPL_INCLUDE_ZPL_ENT_H
 #define ZPL_INCLUDE_ZPL_ENT_H
 
-#ifdef  ZPL_ENT_USE_128_BITS
-#define ZPL_ENT_ID u64
+#ifdef  ZPLE_USE_128_BITS
+#define ZPLE_ID u64
 #else
-#define ZPL_ENT_ID u32
+#define ZPLE_ID u32
 #endif
 
 #if defined(__cplusplus)
 extern "C" {
 #endif
-    
-    typedef struct zpl_ent_id_t {
-        ZPL_ENT_ID id;
-        ZPL_ENT_ID generation;
-    } zpl_ent_id_t, zpl_entity_t;
-    
-    typedef struct zpl_ent_node_t {
-        zpl_ent_id_t           handle;
-        struct zpl_ent_node_t *next;
-    } zpl_ent_node_t;
-    
-    typedef struct zpl_ent_pool {
+
+    typedef struct zple_id_t {
+        ZPLE_ID id;
+        ZPLE_ID generation;
+    } zple_id_t, zple_entity_t;
+
+    typedef struct zple_node_t {
+        zple_id_t           handle;
+        struct zple_node_t *next;
+    } zple_node_t;
+
+    typedef struct zple_pool {
         zpl_allocator_t backing;
         usize           count;
-        zpl_buffer_t(zpl_ent_node_t)freelist;
-        zpl_ent_node_t             *first_free;
-    } zpl_ent_pool;
-    
-    ZPL_DEF void            zpl_ent_init   (zpl_ent_pool *pool, zpl_allocator_t allocator, isize count);
-    ZPL_DEF void            zpl_ent_free   (zpl_ent_pool *pool);
-    ZPL_DEF zpl_ent_id_t    zpl_ent_create (zpl_ent_pool *pool);
-    ZPL_DEF void            zpl_ent_destroy(zpl_ent_pool *pool, zpl_ent_id_t handle);
-    
+        zpl_buffer_t(zple_node_t)freelist;
+        zple_node_t             *first_free;
+    } zple_pool;
+
+    ZPL_DEF void            zple_init   (zple_pool *pool, zpl_allocator_t allocator, isize count);
+    ZPL_DEF void            zple_free   (zple_pool *pool);
+    ZPL_DEF zple_id_t    zple_create (zple_pool *pool);
+    ZPL_DEF void            zple_destroy(zple_pool *pool, zple_id_t handle);
+
     // NOTE(ZaKlaus): To be used as:
     #if 0
 
     // NOTE(ZaKlaus): Header file preferably
     typedef struct {
         i32 x,y,z;
-    } ZPL_ENT_COMP_DECLARE(position);
-    
+    } ZPLE_COMP_DECLARE(position);
+
     // NOTE(ZaKlaus): Exactly 1 occurence in source file
-    ZPL_ENT_COMP_DEFINE(position);
-    
+    ZPLE_COMP_DEFINE(position);
+
     #endif
-#define ZPL_ENT_COMP_DECLARE(NAME) \
+#define ZPLE_COMP_DECLARE(NAME) \
     ZPL_JOIN2(NAME, _t); \
     typedef struct ZPL_JOIN2(NAME, _meta_ent_t) { \
-        zpl_ent_id_t handle; \
-        ZPL_ENT_ID   used; \
+        zple_id_t handle; \
+        ZPLE_ID   used; \
     } ZPL_JOIN2(NAME, _meta_ent_t); \
     typedef struct ZPL_JOIN2(NAME, _pool) { \
         zpl_allocator_t backing; \
@@ -98,14 +95,14 @@ extern "C" {
     } ZPL_JOIN2(NAME, _pool); \
     \
     \
-    void                  ZPL_JOIN2(NAME,_init)       (ZPL_JOIN2(NAME, _pool) *h, zpl_ent_pool *p, zpl_allocator_t a); \
+    void                  ZPL_JOIN2(NAME,_init)       (ZPL_JOIN2(NAME, _pool) *h, zple_pool *p, zpl_allocator_t a); \
     void                  ZPL_JOIN2(NAME,_free)       (ZPL_JOIN2(NAME, _pool) *h); \
-    ZPL_JOIN2(NAME, _t) * ZPL_JOIN2(NAME,_attach)     (ZPL_JOIN2(NAME, _pool) *h, zpl_ent_id_t handle, ZPL_JOIN2(NAME, _t) data); \
-    void                  ZPL_JOIN2(NAME,_detach)     (ZPL_JOIN2(NAME, _pool) *h, zpl_ent_id_t handle); \
-    ZPL_JOIN2(NAME, _t) * ZPL_JOIN2(NAME,_fetch)      (ZPL_JOIN2(NAME, _pool) *h, zpl_ent_id_t handle);
+    ZPL_JOIN2(NAME, _t) * ZPL_JOIN2(NAME,_attach)     (ZPL_JOIN2(NAME, _pool) *h, zple_id_t handle, ZPL_JOIN2(NAME, _t) data); \
+    void                  ZPL_JOIN2(NAME,_detach)     (ZPL_JOIN2(NAME, _pool) *h, zple_id_t handle); \
+    ZPL_JOIN2(NAME, _t) * ZPL_JOIN2(NAME,_fetch)      (ZPL_JOIN2(NAME, _pool) *h, zple_id_t handle);
 
-#define ZPL_ENT_COMP_DEFINE(NAME) \
-    void ZPL_JOIN2(NAME,_init) (ZPL_JOIN2(NAME, _pool) *h, zpl_ent_pool *p, zpl_allocator_t a) { \
+#define ZPLE_COMP_DEFINE(NAME) \
+    void ZPL_JOIN2(NAME,_init) (ZPL_JOIN2(NAME, _pool) *h, zple_pool *p, zpl_allocator_t a) { \
         ZPL_ASSERT(h&&p); h->backing = a; \
         h->count = p->count; \
         zpl_buffer_init(h->entities, a, p->count); \
@@ -116,7 +113,7 @@ extern "C" {
         zpl_buffer_free(h->entities, h->backing); \
         zpl_buffer_free(h->data, h->backing); \
     } \
-    ZPL_JOIN2(NAME, _t) * ZPL_JOIN2(NAME,_attach) (ZPL_JOIN2(NAME, _pool) *h, zpl_ent_id_t handle, ZPL_JOIN2(NAME, _t) data) { \
+    ZPL_JOIN2(NAME, _t) * ZPL_JOIN2(NAME,_attach) (ZPL_JOIN2(NAME, _pool) *h, zple_id_t handle, ZPL_JOIN2(NAME, _t) data) { \
         ZPL_ASSERT(h); \
         ZPL_JOIN2(NAME, _meta_ent_t) *meta_ent = (h->entities+handle.id); \
         meta_ent->handle = handle; \
@@ -124,12 +121,12 @@ extern "C" {
         *(h->data+handle.id) = data; \
         return (h->data+handle.id); \
     } \
-    void ZPL_JOIN2(NAME,_detach) (ZPL_JOIN2(NAME, _pool) *h, zpl_ent_id_t handle) { \
+    void ZPL_JOIN2(NAME,_detach) (ZPL_JOIN2(NAME, _pool) *h, zple_id_t handle) { \
         ZPL_ASSERT(h); \
         ZPL_JOIN2(NAME, _meta_ent_t) *meta_ent = (h->entities+handle.id); \
         meta_ent->used = false; \
     } \
-    ZPL_JOIN2(NAME, _t) * ZPL_JOIN2(NAME, _fetch) (ZPL_JOIN2(NAME, _pool) *h, zpl_ent_id_t handle) { \
+    ZPL_JOIN2(NAME, _t) * ZPL_JOIN2(NAME, _fetch) (ZPL_JOIN2(NAME, _pool) *h, zple_id_t handle) { \
         ZPL_ASSERT(h); \
         ZPL_JOIN2(NAME, _meta_ent_t) *meta_ent = (h->entities+handle.id); \
         if ((meta_ent->used) && (meta_ent->handle.id == handle.id) && (meta_ent->handle.generation == handle.generation)) { \
@@ -139,8 +136,8 @@ extern "C" {
             return NULL; \
         } \
     }
-    
-#define zpl_ent_foreach(pool, comp, cb) \
+
+#define zple_foreach(pool, comp, cb) \
     for (usize i = 0; i < pool->count; ++i) { \
         ZPL_JOIN2(comp, _meta_ent_t) ent = pool->entities[i]; \
         if (ent.used) { \
@@ -148,74 +145,74 @@ extern "C" {
         } \
     }
 
-    
+
 #if defined(__cplusplus)
 }
 #endif
 
-#if defined(ZPL_ENT_IMPLEMENTATION) && !defined(ZPL_ENT_IMPLEMENTATION_DONE)
+#if defined(ZPLE_IMPLEMENTATION) && !defined(ZPLE_IMPLEMENTATION_DONE)
 
 #if defined(__cplusplus)
 extern "C" {
 #endif
 
-    void zpl_ent_init(zpl_ent_pool *pool, zpl_allocator_t allocator, isize count) {
+    void zple_init(zple_pool *pool, zpl_allocator_t allocator, isize count) {
         ZPL_ASSERT(pool);
-        
+
         zpl_allocator_t a = pool->backing = allocator;
         pool->count = count;
-        
-        zpl_buffer_init(pool->freelist, a, zpl_size_of(zpl_ent_node_t)*count);
-        
+
+        zpl_buffer_init(pool->freelist, a, zpl_size_of(zple_node_t)*count);
+
         // NOTE(ZaKlaus): Build the freelist
         pool->first_free = pool->freelist;
         for (usize i = 0; i < count; ++i) {
-            zpl_ent_node_t *f = (pool->freelist+i);
-            f->handle = (zpl_ent_id_t) { i, 0 };
+            zple_node_t *f = (pool->freelist+i);
+            f->handle = (zple_id_t) { i, 0 };
             f->next = (pool->freelist+i+1);
         }
         (pool->freelist+count-1)->next = NULL;
     }
-    
-    void zpl_ent_free(zpl_ent_pool *pool) {
+
+    void zple_free(zple_pool *pool) {
         ZPL_ASSERT(pool);
-        
+
         zpl_buffer_free(pool->freelist, pool->backing);
     }
-    
-    zpl_ent_id_t zpl_ent_create(zpl_ent_pool *pool) {
+
+    zple_id_t zple_create(zple_pool *pool) {
         ZPL_ASSERT(pool);
         ZPL_ASSERT(pool->first_free);
-        
-        zpl_ent_node_t *f = pool->first_free;
-        zpl_ent_id_t ent = f->handle;
+
+        zple_node_t *f = pool->first_free;
+        zple_id_t ent = f->handle;
         pool->first_free = f->next;
-        
+
         return ent;
     }
-    
-    void zpl_ent_destroy(zpl_ent_pool *pool, zpl_ent_id_t handle) {
+
+    void zple_destroy(zple_pool *pool, zple_id_t handle) {
         ZPL_ASSERT(pool);
-        
+
         if (pool->first_free == NULL) {
-            zpl_ent_node_t *f = pool->first_free = pool->freelist;
+            zple_node_t *f = pool->first_free = pool->freelist;
             f->handle = handle;
             f->next   = NULL;
             ++f->handle.generation;
         }
         else {
             ZPL_ASSERT(pool->first_free - 1 >= pool->freelist);
-            zpl_ent_node_t f_ = { (zpl_ent_id_t){ handle.id, handle.generation+1 }, pool->first_free };
-            zpl_ent_node_t *f = (pool->first_free - 1);
+            zple_node_t f_ = { (zple_id_t){ handle.id, handle.generation+1 }, pool->first_free };
+            zple_node_t *f = (pool->first_free - 1);
             *f = f_;
             pool->first_free = f;
         }
     }
-    
+
 #if defined(__cplusplus)
 }
 #endif
 
 #endif
-        
+
 #endif // ZPL_INCLUDE_ZPL_ENT_H
--- test/entity_hero.c
+++ test/entity_hero.c
@@ -1,22 +1,22 @@
 #define ZPL_IMPLEMENTATION
-#define ZPL_ENT_IMPLEMENTATION
+#define ZPLE_IMPLEMENTATION
 #include <zpl.h>
 #include <zpl_ent.h>
 
 typedef struct {
     zpl_string_t name;
     i32 age;
-} ZPL_ENT_COMP_DECLARE(persona);
+} ZPLE_COMP_DECLARE(persona);
 
 typedef struct {
     i32 x,y,z;
-} ZPL_ENT_COMP_DECLARE(position);
+} ZPLE_COMP_DECLARE(position);
 
 typedef struct {
     f32 strength;
     f32 wisdom;
     f32 agility;
-} ZPL_ENT_COMP_DECLARE(stats);
+} ZPLE_COMP_DECLARE(stats);
 
 // NOTE(ZaKlaus): Demonstration purposes
 typedef void armor_piece_t;
@@ -25,11 +25,11 @@ typedef struct {
     armor_piece_t *Head;
     armor_piece_t *Torso;
     armor_piece_t *Legs;
-} ZPL_ENT_COMP_DECLARE(armor);
+} ZPLE_COMP_DECLARE(armor);
 
 typedef struct {
-    zpl_ent_pool entities;
-    
+    zple_pool entities;
+
     // NOTE(ZaKlaus): All possible components
     persona_pool  persona;
     position_pool position;
@@ -38,15 +38,15 @@ typedef struct {
 } world_t;
 
 //// NOTE(ZaKlaus): These are usually placed in exactly ONE source file
-ZPL_ENT_COMP_DEFINE(persona);
-ZPL_ENT_COMP_DEFINE(position);
-ZPL_ENT_COMP_DEFINE(stats);
-ZPL_ENT_COMP_DEFINE(armor);
+ZPLE_COMP_DEFINE(persona);
+ZPLE_COMP_DEFINE(position);
+ZPLE_COMP_DEFINE(stats);
+ZPLE_COMP_DEFINE(armor);
 ////
 
 void world_init(world_t *w, zpl_allocator_t alloc, isize count) {
-    zpl_ent_init(&w->entities, alloc, count);
-    
+    zple_init(&w->entities, alloc, count);
+
     persona_init(&w->persona, &w->entities, alloc);
     position_init(&w->position, &w->entities, alloc);
     stats_init(&w->stats, &w->entities, alloc);
@@ -54,8 +54,8 @@ void world_init(world_t *w, zpl_allocator_t alloc, isize count) {
 }
 
 void world_free(world_t *w) {
-    zpl_ent_free(&w->entities);
-    
+    zple_free(&w->entities);
+
     persona_free(&w->persona);
     position_free(&w->position);
     stats_free(&w->stats);
@@ -63,32 +63,32 @@ void world_free(world_t *w) {
 }
 
 
-zpl_entity_t hero_spawn(world_t *world, char *const name, i32 age, position_t pos, f32 strength_factor) {
-    zpl_entity_t hero = zpl_ent_create(&world->entities);
-    
+zple_entity_t hero_spawn(world_t *world, char *const name, i32 age, position_t pos, f32 strength_factor) {
+    zple_entity_t hero = zple_create(&world->entities);
+
     persona_attach(&world->persona, hero, (persona_t){ zpl_string_make(zpl_heap_allocator(), name), age, });
     position_attach(&world->position, hero, pos);
     armor_attach(&world->armor, hero, (armor_t){0});
-    
+
     stats_t stats = { strength: 2*strength_factor, wisdom: 7*(age/cast(f32)12), agility: 6*(8.0/age) };
     stats_attach(&world->stats, hero, stats);
-    
+
     return hero;
 }
 
-void hero_kill(world_t *world, zpl_entity_t hero) {
+void hero_kill(world_t *world, zple_entity_t hero) {
     persona_detach(&world->persona, hero);
     position_detach(&world->position, hero);
     stats_detach(&world->stats, hero);
     armor_detach(&world->armor, hero);
-    
-    zpl_ent_destroy(&world->entities, hero);
+
+    zple_destroy(&world->entities, hero);
 }
 
-void hero_dostuff(world_t *world, zpl_entity_t hero) {
+void hero_dostuff(world_t *world, zple_entity_t hero) {
     zpl_when (persona_fetch(&world->persona, hero), persona_t*, persona) {
         stats_t *stats = stats_fetch(&world->stats, hero);
-        
+
         zpl_printf("Hero called %s and aged %d with strength: %d, wisdom: %d and agility: %d was here!\n",
                    persona->name, persona->age, stats->strength, stats->wisdom, stats->agility);
     }
@@ -103,22 +103,22 @@ char *names[] = {
 };
 
 int main(void) {
-    
+
     world_t world = {0};
     world_init(&world, zpl_heap_allocator(), 5);
-    
+
     // NOTE(ZaKlaus): Our heroes will do stuff
     {
         for (isize i = 0; i < 5; ++i) {
-            zpl_entity_t hero = hero_spawn(&world, names[i], (25 + (i%3)*4), (position_t){ i, i*2, i*i }, i % 4);
-            
+            zple_entity_t hero = hero_spawn(&world, names[i], (25 + (i%3)*4), (position_t){ i, i*2, i*i }, i % 4);
+
             hero_dostuff(&world, hero);
-            
+
             hero_kill(&world, hero);
         }
     }
-    
+
     world_free(&world);
-    
+
     return 0;
 }
