6ef476dcc0c0d38690c30139aa0c940355a50e76
Dominik Madarász <zaklaus@outlook.com>
2018-03-07 18:49:39 +0100

    Updated MSVC build script

--- misc/scripts/build.bat
+++ misc/scripts/build.bat
@@ -1,19 +1,7 @@
 @echo off
 
-set CommonCompilerFlags=-Od -MTd -nologo -fp:fast -fp:except- -Gm- -GR- -EHa- -Zo -Oi -W4 -wd4201 -wd4100 -wd4189 -wd4505 -wd4204 -wd4067 -wd4057 -wd4244 -wd4456 -wd4706 -wd4127 -FC -Z7
-set CommonLinkerFlags= -incremental:no -opt:ref user32.lib gdi32.lib winmm.lib opengl32.lib
+set WARN=/W3 /WX
+set CMN=-MTd -nologo -fp:fast -fp:except- -Gm- -GR- -EHa- -Zo -FC -Z7
+set OPTS=/Od
 
-IF "%FRAGWORK_STREAMING%"=="" goto nostreaming
-set CommonCompilerFlags=%CommonCompilerFlags% -DFRAGWORK_STREAMING=1
-:nostreaming
-
-REM TODO - can we just build both with one exe?
-
-IF NOT EXIST ..\build mkdir ..\build
-pushd ..\build
-
-del *.pdb > NUL 2> NUL
-
-cl %CommonCompilerFlags% ..\code\test.c /link %CommonLinkerFlags%
-
-popd
+cl %1.c %WARN% %OPTS% %CMN% /I..\code
