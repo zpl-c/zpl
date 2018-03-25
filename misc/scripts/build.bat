09bc8ffcec13e4cb2e39c814c3c75963a2197891
Dominik Madarász <zaklaus@outlook.com>
2018-03-25 12:06:39 +0200

    Ported platform layer from https://github.com/gingerBill/gb/

--- misc/scripts/build.bat
+++ misc/scripts/build.bat
@@ -3,7 +3,9 @@
 set WARN=/W3 /WX
 set CMN=-MTd -nologo -fp:fast -fp:except- -Gm- -GR- -EHa- -Zo -FC -Z7
 set OPTS=/Od
+set LIBS=user32.lib gdi32.lib winmm.lib opengl32.lib kernel32.lib
+set SUBSYSTEM=windows
 
 ctime -begin quick.ctm
-cl %1 %WARN% %OPTS% %CMN% /I..\code
+cl %1 %WARN% %OPTS% %CMN% /I..\code /link /SUBSYSTEM:%SUBSYSTEM% %LIBS%
 ctime -end quick.ctm
