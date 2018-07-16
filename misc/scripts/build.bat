8ed7d5dba6e704a1b9c23c2187f5c4a61b689f2e
Dominik Madarász <zaklaus@outlook.com>
2018-07-16 12:42:37 +0200

    Update build.bat

--- misc/scripts/build.bat
+++ misc/scripts/build.bat
@@ -1,10 +1,13 @@
 @echo off
+SETLOCAL
 
 set WARN=/W3 /WX
 set CMN=-MTd -nologo -fp:fast -fp:except- -Gm- -GR- -EHa- -Zo -FC -Z7
 set OPTS=/Od
 set LIBS=user32.lib gdi32.lib winmm.lib opengl32.lib kernel32.lib
-set SUBSYSTEM=windows
+set SUBSYSTEM=console
+
+python.exe w:\zpl\code\zpl\build.py
 
 ctime -begin quick.ctm
 cl %1 %WARN% %OPTS% %CMN% /I..\code /link /SUBSYSTEM:%SUBSYSTEM% %LIBS%
