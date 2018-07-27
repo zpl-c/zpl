e62e561ad1a1107174a82fd7026a4fc636aa131c
Dominik Madarász <zaklaus@outlook.com>
2018-07-27 02:32:29 +0200

    Updated build scripts

--- misc/scripts/build_msvc.bat
+++ misc/scripts/build_msvc.bat
@@ -4,15 +4,15 @@ SETLOCAL
 if not exist bin mkdir bin
 
 pushd bin
-set WARN=-Wall -Wextra
-set CMN=-g --std=c11
-set OPTS=
+set WARN=/W3 /WX
+set CMN=-MTd -nologo -fp:fast -fp:except- -Gm- -GR- -EHa- -Zo -FC -Z7
+set OPTS=/Od
 set LIBS=user32.lib gdi32.lib winmm.lib opengl32.lib kernel32.lib
 set SUBSYSTEM=console
 
 python.exe w:\zpl\code\zpl\build.py
 
 ctime -begin quick.ctm
-clang ..\%1 %WARN% %OPTS% %CMN% -I..\..\code
+cl ..\%1 %WARN% %OPTS% %CMN% /I..\..\code /link /SUBSYSTEM:%SUBSYSTEM% %LIBS%
 ctime -end quick.ctm
 popd
