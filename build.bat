7f609b5d38e744557ce852105029c610be75d824
Dominik Madarász <zaklaus@outlook.com>
2018-10-08 19:18:17 +0200

    Make build script configurable.

--- build.bat
+++ build.bat
@@ -1,6 +1,8 @@
 @echo off
 SETLOCAL
 
+REM build.bat [file_name] [OPTS] [LIBS] [INCLUDE PATHS] [LIB PATHS] [SUBSYSTEM] [CPP_EXTRAS] [LNK_EXTRAS]
+
 if not exist build\bin mkdir build\bin
 
 call generate.bat
@@ -12,15 +14,15 @@ set OPTS=%2
 
 IF [%2] == [] set OPTS=/Od
 
-set LIBS=%3
+set LIBS=%~3
 
 IF [%3] == [] set LIBS=user32.lib gdi32.lib winmm.lib opengl32.lib kernel32.lib vulkan-1.lib glfw3.lib shell32.lib
 
-set INCLUDEPATHS=%4
+set INCLUDEPATHS=%~4
 
 IF [%4] == [] set INCLUDEPATHS=/I..\..\code /IC:\VulkanSDK\1.1.82.1\Include /IC:\glfw\include
 
-set LIBPATHS=%5
+set LIBPATHS=%~5
 
 IF [%5] == [] set LIBPATHS=/LIBPATH:C:\VulkanSDK\1.1.82.1\Lib /LIBPATH:C:\glfw\lib-vc2015
 
@@ -28,8 +30,8 @@ set SUBSYSTEM=%6
 
 IF [%6] == [] set SUBSYSTEM=console
 
-set CPP_EXTRAS=%~dp7
-set LNK_EXTRAS=%~dp8
+set CPP_EXTRAS=%~7
+set LNK_EXTRAS=%~8
 
 ctime -begin build.ctm
 cl %1 %WARN% %OPTS% %CMN% %INCLUDEPATHS% %CPP_EXTRAS% /link %LIBPATHS% /NODEFAULTLIB:glfw3 /SUBSYSTEM:%SUBSYSTEM% %LNK_EXTRAS% %LIBS%
--- build_tests.bat
+++ build_tests.bat
@@ -2,5 +2,5 @@
 
 for %%f in (test\*.c) do (
     echo %%~f
-	call build.bat ..\..\%%~f /Od
+	call build.bat ..\..\%%~f /Od "user32.lib winmm.lib kernel32.lib" "/I..\..\code"
 )
