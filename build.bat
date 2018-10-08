781c72cf9b5e4b3315f5778e6103340075afae9c
Dominik Madarász <zaklaus@outlook.com>
2018-10-08 19:11:13 +0200

    Update build script

--- build.bat
+++ build.bat
@@ -12,12 +12,26 @@ set OPTS=%2
 
 IF [%2] == [] set OPTS=/Od
 
-set LIBS=user32.lib gdi32.lib winmm.lib opengl32.lib kernel32.lib vulkan-1.lib glfw3.lib shell32.lib
-set SUBSYSTEM=console
-set INCLUDEPATHS=/I..\..\code /IC:\VulkanSDK\1.1.82.1\Include /IC:\glfw\include
-set LIBPATHS=/LIBPATH:C:\VulkanSDK\1.1.82.1\Lib /LIBPATH:C:\glfw\lib-vc2015
+set LIBS=%3
+
+IF [%3] == [] set LIBS=user32.lib gdi32.lib winmm.lib opengl32.lib kernel32.lib vulkan-1.lib glfw3.lib shell32.lib
+
+set INCLUDEPATHS=%4
+
+IF [%4] == [] set INCLUDEPATHS=/I..\..\code /IC:\VulkanSDK\1.1.82.1\Include /IC:\glfw\include
+
+set LIBPATHS=%5
+
+IF [%5] == [] set LIBPATHS=/LIBPATH:C:\VulkanSDK\1.1.82.1\Lib /LIBPATH:C:\glfw\lib-vc2015
+
+set SUBSYSTEM=%6
+
+IF [%6] == [] set SUBSYSTEM=console
+
+set CPP_EXTRAS=%~dp7
+set LNK_EXTRAS=%~dp8
 
 ctime -begin build.ctm
-cl %1 %WARN% %OPTS% %CMN% %INCLUDEPATHS% /link %LIBPATHS% /NODEFAULTLIB:glfw3 /SUBSYSTEM:%SUBSYSTEM% %LIBS%
+cl %1 %WARN% %OPTS% %CMN% %INCLUDEPATHS% %CPP_EXTRAS% /link %LIBPATHS% /NODEFAULTLIB:glfw3 /SUBSYSTEM:%SUBSYSTEM% %LNK_EXTRAS% %LIBS%
 ctime -end build.ctm
 popd
