4338b3de9e3e13a14d45e04d94032496bfd0eaaa
Dominik Madarász <zaklaus@outlook.com>
2019-06-23 13:14:23 +0200

    Who forgot to push this? me!

--- code/zpl/__main__.c
+++ code/zpl/__main__.c
@@ -26,6 +26,7 @@ GitHub:
   https://github.com/zpl-c/zpl
   
 Version History: 
+  9.4.5 - Mistakes were made, fixed compilation errors
   9.4.3 - Fix old API shenanigans
   9.4.2 - Fix small API typos
   9.4.1 - Reordered JSON5 constants to integrate better with conditions
