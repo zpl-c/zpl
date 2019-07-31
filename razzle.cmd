4e726e2231de99c9e3fc4b202c056b0c622f6d0b
Dominik Madarász <zaklaus@outlook.com>
2019-07-31 13:25:08 +0200

    Update razzle
    Update razzle.cmd to support VS17 and VS19 Community editions.

--- razzle.cmd
+++ razzle.cmd
@@ -20,4 +20,9 @@ IF NOT DEFINED LIB (IF EXIST "%VC_PATH%" (call "%VC_PATH%\VC\vcvarsall.bat" %1))
 SET VC_PATH=C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise
 IF NOT DEFINED LIB (IF EXIST "%VC_PATH%" (call "%VC_PATH%\VC\Auxiliary\Build\vcvarsall.bat" %1))
 
+SET VC_PATH=C:\Program Files (x86)\Microsoft Visual Studio\2017\Community
+IF NOT DEFINED LIB (IF EXIST "%VC_PATH%" (call "%VC_PATH%\VC\Auxiliary\Build\vcvarsall.bat" %1))
+
+SET VC_PATH=C:\Program Files (x86)\Microsoft Visual Studio\2019\Community
+IF NOT DEFINED LIB (IF EXIST "%VC_PATH%" (call "%VC_PATH%\VC\Auxiliary\Build\vcvarsall.bat" %1))
 
