267d17488a89bf0434a2dcf64ecab1ef5619c184
Dominik Madarász <zaklaus@outlook.com>
2019-08-01 22:06:29 +0200

    Update README.md

--- razzle.cmd
+++ razzle.cmd
@@ -25,4 +25,3 @@ IF NOT DEFINED LIB (IF EXIST "%VC_PATH%" (call "%VC_PATH%\VC\Auxiliary\Build\vcv
 
 SET VC_PATH=C:\Program Files (x86)\Microsoft Visual Studio\2019\Community
 IF NOT DEFINED LIB (IF EXIST "%VC_PATH%" (call "%VC_PATH%\VC\Auxiliary\Build\vcvarsall.bat" %1))
-
