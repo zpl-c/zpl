0fecf9372a0d2a26d4e2c4215bb7f698a8fdb42b
Dominik Madarász <zaklaus@outlook.com>
2019-10-17 07:29:05 +0200

    Additional CI fixes

--- razzle.cmd
+++ razzle.cmd
@@ -29,5 +29,5 @@ IF NOT DEFINED LIB (IF EXIST "%VC_PATH%" (call "%VC_PATH%\VC\Auxiliary\Build\vcv
 SET VC_PATH=C:\Program Files (x86)\Microsoft Visual Studio\2019\Community
 IF NOT DEFINED LIB (IF EXIST "%VC_PATH%" (call "%VC_PATH%\VC\Auxiliary\Build\vcvarsall.bat" %1))
 
-SET VC_PATH="C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise"
+SET VC_PATH=C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise
 IF NOT DEFINED LIB (IF EXIST "%VC_PATH%" (call "%VC_PATH%\Common7\Tools\VsDevCmd.bat"))
