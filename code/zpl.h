08c40cf0f543f9f9955d5361087743c1eac30857
Vladyslav Hrytsenko <vladgritsenko@gmail.com>
2020-02-03 14:44:04 +0200

    Release 10.0.9

--- code/zpl.h
+++ code/zpl.h
@@ -246,7 +246,7 @@ Version History:
 
 #define ZPL_VERSION_MAJOR 10
 #define ZPL_VERSION_MINOR 0
-#define ZPL_VERSION_PATCH 8
+#define ZPL_VERSION_PATCH 9
 #define ZPL_VERSION_PRE ""
 
 #include "zpl_hedley.h"
