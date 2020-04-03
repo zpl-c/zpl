5e16fca45db33905bd78c4317a71a9c5ef6155f4
Dominik Madarasz <zaklaus@outlook.com>
2020-04-03 12:21:31 +0200

    Release 10.2.0

--- code/zpl.h
+++ code/zpl.h
@@ -253,7 +253,7 @@ Version History:
 #define ZPL_H
 
 #define ZPL_VERSION_MAJOR 10
-#define ZPL_VERSION_MINOR 1
+#define ZPL_VERSION_MINOR 2
 #define ZPL_VERSION_PATCH 0
 #define ZPL_VERSION_PRE ""
 
