85e710c7ad08d1891a908fde3f276743dc3a555a
Dominik Madarász <zaklaus@outlook.com>
2020-06-03 16:27:20 +0200

    Meta: Release bump

--- code/zpl.h
+++ code/zpl.h
@@ -27,6 +27,7 @@ GitHub:
   https://github.com/zpl-c/zpl
 
 Version History:
+  10.5.3  - Removed crc64 and annotated some hashing methods
   10.5.2  - Don't expose ZPL types anymore
   10.5.1  - Fixed zpl_rdtsc for Emscripten
   10.5.0  - Changed casts to memcopy in random methods, added embed cmd
