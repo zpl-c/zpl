9c49990cbaaf83a5d9c4ae143dd71696e723dfd2
Dominik Madarász <zaklaus@outlook.com>
2020-06-03 21:22:59 +0200

    Release 10.5.6

--- code/zpl.h
+++ code/zpl.h
@@ -265,7 +265,7 @@ Version History:
 
 #define ZPL_VERSION_MAJOR 10
 #define ZPL_VERSION_MINOR 5
-#define ZPL_VERSION_PATCH 5
+#define ZPL_VERSION_PATCH 6
 #define ZPL_VERSION_PRE ""
 
 #include "zpl_hedley.h"
