b45b42c2fb70d247b49d1247947ac2c7c637a546
Dominik Madarász <zaklaus@outlook.com>
2021-01-05 19:37:47 +0100

    Release 10.11.0

--- code/zpl.h
+++ code/zpl.h
@@ -294,8 +294,8 @@ Version History:
 #define ZPL_H
 
 #define ZPL_VERSION_MAJOR 10
-#define ZPL_VERSION_MINOR 10
-#define ZPL_VERSION_PATCH 4
+#define ZPL_VERSION_MINOR 11
+#define ZPL_VERSION_PATCH 0
 #define ZPL_VERSION_PRE ""
 
 #include "zpl_hedley.h"
