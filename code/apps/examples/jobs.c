c9350a44cde7834b8c61ab0cc2520f3a4fb5c068
Dominik Madarász <zaklaus@outlook.com>
2021-01-15 13:18:30 +0100

    jobs: fix deadlock on term request

--- code/apps/examples/jobs.c
+++ code/apps/examples/jobs.c
@@ -1,58 +1,61 @@
 #define ZPL_IMPLEMENTATION
 #define ZPL_NANO
-#define ZPL_ENABLE_THREADING
 #define ZPL_ENABLE_JOBS
 #include <zpl.h>
-#define TEST_ENQUEUE_JOB 0.8
+#include <stdlib.h>
+
+#define N 30
+#define L 0.5
+#define H 1.0
+#define NL 30
+#define NH 50
+#define CORES 2
+#define RAND_RANGE(min,max) (min + rand() % (max-min))
+
+zpl_global zpl_u8 counter = 0;
 
 #if defined(ZPL_MODULE_THREADING)
-zpl_mutex print_mut;
 
-void calc_nums(void* data) {
+void work_low(void *data) {
     zpl_unused(data);
-    zpl_i64 nums=0;
-    zpl_random rnd={0};
-    zpl_random_init(&rnd);
+    zpl_sleep_ms(RAND_RANGE(5, 10));
+}
 
-    for (int i=0; i<100; ++i) {
-        nums+=(zpl_random_gen_u64(&rnd) & 100);
-    }
-
-    //zpl_sleep_ms(50*zpl_random_range_i64(&rnd, 2, 8));
-
-    zpl_mutex_lock(&print_mut);
-    zpl_printf("Result is: %ld\n", (long)nums);
-    zpl_mutex_unlock(&print_mut);
+void work_high(void *data) {
+    zpl_unused(data);
+    zpl_sleep_ms(RAND_RANGE(1, 2));
 }
 
 int main() {
-    zpl_random rng={0};
     zpl_thread_pool p={0};
-    zpl_jobs_init(&p, zpl_heap(), 2);
-    zpl_random_init(&rng);
-    zpl_mutex_init(&print_mut);
+    zpl_jobs_init(&p, zpl_heap(), CORES);
+    zpl_u64 process_time = 0, avg_delta_time = 0;
 
-    zpl_jobs_enqueue(&p, calc_nums, NULL);
-    zpl_jobs_enqueue(&p, calc_nums, NULL);
-    zpl_jobs_enqueue(&p, calc_nums, NULL);
-    zpl_jobs_enqueue(&p, calc_nums, NULL);
-    zpl_jobs_enqueue(&p, calc_nums, NULL);
-    zpl_jobs_enqueue(&p, calc_nums, NULL);
-    zpl_jobs_enqueue(&p, calc_nums, NULL);
+    zpl_printf("Jobs test, %d iterations ran on %d cores.\nPer each iteration, we spawn:\n* %d low-priority jobs.\n* %d high-priority jobs.\n", N, CORES, NL, NH);
 
-    zpl_f64 time=zpl_time_rel();
-
-    for (;;) {
-        zpl_f64 now=zpl_time_rel();
-        zpl_f64 dt =now-time;
-        if (dt > TEST_ENQUEUE_JOB) {
-            time=now;
-            zpl_jobs_enqueue(&p, calc_nums, NULL);
+    while (counter < N) {
+        if (zpl_jobs_empty(&p)) {
+            counter++;
+            zpl_printf("Iterations left: %d.      \r", (N-counter));
+            for (int i=0; i<NL; i++) {
+                zpl_jobs_enqueue_with_priority(&p, work_low, 0, L);
+            }
+            for (int i=0; i<NH; i++) {
+                zpl_jobs_enqueue_with_priority(&p, work_high, 0, H);
+            }
+        } else {
+            zpl_u64 curr_time = zpl_time_rel_ms();
+            zpl_jobs_process(&p);
+            zpl_u64 delta_time = zpl_time_rel_ms() - curr_time;
+            avg_delta_time += delta_time;
+            process_time += delta_time;
         }
-
-        zpl_jobs_process(&p);
     }
 
+    avg_delta_time /= N;
+    zpl_printf("\nDone!\n");
+    zpl_jobs_free(&p);
+    zpl_printf("Test is done, results:\n* %lld ms process time (overall).\n* %lld ms process time (average).\n", process_time, avg_delta_time);
     return 0;
 }
 #else
