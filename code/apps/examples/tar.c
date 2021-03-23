9c37f3ab955cb450183629d0143967f99e4fbe0f
Dominik Madarász <zaklaus@outlook.com>
2021-03-23 17:58:27 +0100

    code: provide tar unpack defaults

--- code/apps/examples/tar.c
+++ code/apps/examples/tar.c
@@ -2,8 +2,9 @@
 #define ZPL_NANO
 #include <zpl.h>
 
-zpl_isize print_files(zpl_file *archive, zpl_tar_record *file) {
+zpl_isize print_files(zpl_file *archive, zpl_tar_record *file, void* user_data) {
     (void)archive;
+    (void)user_data;
     if (file->error != ZPL_TAR_ERROR_NONE)
         return 0; /* skip file */
 
@@ -30,7 +31,9 @@ int main() {
 
     zpl_file b;
     zpl_file_open(&b, "misc/data/test_archive.tar");
-    zpl_tar_unpack(&b, print_files);
+    zpl_tar_unpack(&b, zpl_tar_default_list_file, 0);
+    zpl_file_seek(&b, 0);
+    zpl_tar_unpack(&b, zpl_tar_default_unpack_file, "build");
     zpl_file_close(&b);
     return 0;
 }
