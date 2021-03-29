2a246ed719d96fe495829b9beb50f02264a7707f
Dominik Madarász <zaklaus@outlook.com>
2021-03-29 16:41:39 +0200

    Release 12.4.0

--- code/zpl.h
+++ code/zpl.h
@@ -41,8 +41,8 @@ License:
 #define ZPL_H
 
 #define ZPL_VERSION_MAJOR 12
-#define ZPL_VERSION_MINOR 3
-#define ZPL_VERSION_PATCH 2
+#define ZPL_VERSION_MINOR 4
+#define ZPL_VERSION_PATCH 0
 #define ZPL_VERSION_PRE ""
 
 #include "zpl_hedley.h"
