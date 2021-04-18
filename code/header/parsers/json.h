de3df8df21dd26df6cc5731b042fe012c46cf523
Dominik Madarász <zaklaus@outlook.com>
2021-04-18 21:14:58 +0200

    json: further cleanup

--- code/header/parsers/json.h
+++ code/header/parsers/json.h
@@ -10,12 +10,11 @@ typedef enum zpl_json_error {
     ZPL_JSON_ERROR_NONE,
     ZPL_JSON_ERROR_INVALID_NAME,
     ZPL_JSON_ERROR_INVALID_VALUE,
-    ZPL_JSON_ERROR_OBJECT_OR_SOURCE_WAS_NULL,
 } zpl_json_error;
 
 typedef zpl_ast_node zpl_json_object;
 
-ZPL_DEF zpl_u8 zpl_json_parse(zpl_json_object *root, char *source, zpl_allocator allocator);
+ZPL_DEF zpl_u8 zpl_json_parse(zpl_json_object *root, char *text, zpl_allocator allocator);
 ZPL_DEF void zpl_json_free(zpl_json_object *obj);
 ZPL_DEF void zpl_json_write(zpl_file *file, zpl_json_object *obj, zpl_isize indent);
 ZPL_DEF zpl_string zpl_json_write_string(zpl_allocator a, zpl_json_object *obj, zpl_isize indent);
