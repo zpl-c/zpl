4c6c618fe8ed891dd51cfe7a2ee6c7fe93e1e2d6
Dominik Madarász <zaklaus@outlook.com>
2021-04-19 10:55:30 +0200

    json: drop math.h dep

--- code/header/core/system.h
+++ code/header/core/system.h
@@ -155,4 +155,12 @@ defined(__ppc64__) || defined(__aarch64__)
     #endif
 #endif
 
+#if ZPL_GNUC_VERSION_CHECK(3, 3, 0)
+    #define ZPL_INFINITY (__builtin_inff())
+    #define ZPL_NAN (__builtin_nanf(""))
+#else
+    #define ZPL_INFINITY (1e10000f)
+    #define ZPL_NAN (0.0f / 0.0f))
+#endif
+
 ZPL_END_C_DECLS
