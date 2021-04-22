7666f86784d173d07b8b1653d33ddb8d591c9e5c
Dominik Madarász <zaklaus@outlook.com>
2021-04-22 18:49:57 +0200

    Release 13.1.1

--- code/zpl.h
+++ code/zpl.h
@@ -42,7 +42,7 @@ License:
 
 #define ZPL_VERSION_MAJOR 13
 #define ZPL_VERSION_MINOR 1
-#define ZPL_VERSION_PATCH 0
+#define ZPL_VERSION_PATCH 1
 #define ZPL_VERSION_PRE ""
 
 #include "zpl_hedley.h"
