e4a8bab7b8fe678538fbcdd4e69f6837aafac3af
Dominik Madarász <zaklaus@outlook.com>
2021-05-03 16:09:41 +0200

    win: setup vs2019 first

--- misc/win/setup_cl_generic.bat
+++ misc/win/setup_cl_generic.bat
@@ -5,6 +5,13 @@ IF %ERRORLEVEL% == 0 (EXIT /b)
 
 SET "LIB="
 
+
+SET VC_PATH=C:\Program Files (x86)\Microsoft Visual Studio 14.0
+IF NOT DEFINED LIB (IF EXIST "%VC_PATH%" (call "%VC_PATH%\VC\vcvarsall.bat" %1))
+
+SET VC_PATH=C:\Program Files (x86)\Microsoft Visual Studio 13.0
+IF NOT DEFINED LIB (IF EXIST "%VC_PATH%" (call "%VC_PATH%\VC\vcvarsall.bat" %1))
+
 SET VC_PATH=C:\Program Files (x86)\Microsoft Visual Studio 12.0
 IF NOT DEFINED LIB (IF EXIST "%VC_PATH%" (call "%VC_PATH%\VC\vcvarsall.bat" %1))
 
@@ -14,11 +21,11 @@ IF NOT DEFINED LIB (IF EXIST "%VC_PATH%" (call "%VC_PATH%\VC\vcvarsall.bat" %1))
 SET VC_PATH=C:\Program Files (x86)\Microsoft Visual Studio 10.0
 IF NOT DEFINED LIB (IF EXIST "%VC_PATH%" (call "%VC_PATH%\VC\vcvarsall.bat" %1))
 
-SET VC_PATH=C:\Program Files (x86)\Microsoft Visual Studio 13.0
-IF NOT DEFINED LIB (IF EXIST "%VC_PATH%" (call "%VC_PATH%\VC\vcvarsall.bat" %1))
+SET VC_PATH=C:\Program Files (x86)\Microsoft Visual Studio\2019\Community
+IF NOT DEFINED LIB (IF EXIST "%VC_PATH%" (call "%VC_PATH%\VC\Auxiliary\Build\vcvarsall.bat" %1))
 
-SET VC_PATH=C:\Program Files (x86)\Microsoft Visual Studio 14.0
-IF NOT DEFINED LIB (IF EXIST "%VC_PATH%" (call "%VC_PATH%\VC\vcvarsall.bat" %1))
+SET VC_PATH=C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise
+IF NOT DEFINED LIB (IF EXIST "%VC_PATH%" (call "%VC_PATH%\Common7\Tools\VsDevCmd.bat"))
 
 SET VC_PATH=C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise
 IF NOT DEFINED LIB (IF EXIST "%VC_PATH%" (call "%VC_PATH%\VC\Auxiliary\Build\vcvarsall.bat" %1))
@@ -28,9 +35,3 @@ IF NOT DEFINED LIB (IF EXIST "%VC_PATH%" (call "%VC_PATH%\VC\Auxiliary\Build\vcv
 
 SET VC_PATH=C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools
 IF NOT DEFINED LIB (IF EXIST "%VC_PATH%" (call "%VC_PATH%\VC\Auxiliary\Build\vcvarsall.bat" %1))
-
-SET VC_PATH=C:\Program Files (x86)\Microsoft Visual Studio\2019\Community
-IF NOT DEFINED LIB (IF EXIST "%VC_PATH%" (call "%VC_PATH%\VC\Auxiliary\Build\vcvarsall.bat" %1))
-
-SET VC_PATH=C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise
-IF NOT DEFINED LIB (IF EXIST "%VC_PATH%" (call "%VC_PATH%\Common7\Tools\VsDevCmd.bat"))
