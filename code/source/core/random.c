9cdd9ee33d20e6e682cd75f5d20490a1a60c4b51
Dominik Madarász <zaklaus@madaraszd.net>
2021-09-26 14:51:29 +0200

    Fix zpl_random_range_i64
    For a range <0;1>, the following issue was perceived:
    
    If N is negative odd number, N % 2 returns -1, which is outside the bounds of selected range.
    
    This fixes the issue by performing modulo operation on an unsigned integer instead, then converting to i64 and adding lower_inc.

--- code/source/core/random.c
+++ code/source/core/random.c
@@ -4,20 +4,20 @@
 ZPL_BEGIN_C_DECLS
 
 #if defined(ZPL_MODULE_THREADING)
-    zpl_global zpl_atomic32 zpl__random_shared_counter = {0};
+zpl_global zpl_atomic32 zpl__random_shared_counter = {0};
 #else
-    zpl_global zpl_i32 zpl__random_shared_counter = 0;
+zpl_global zpl_i32 zpl__random_shared_counter = 0;
 #endif
 
 zpl_internal zpl_u32 zpl__get_noise_from_time(void) {
     zpl_u32 accum = 0;
     zpl_f64 start, remaining, end, curr = 0;
     zpl_u64 interval = 100000ll;
-
+    
     start     = zpl_time_rel();
     remaining = (interval - cast(zpl_u64)(interval*start)%interval) / cast(zpl_f64)interval;
     end       = start + remaining;
-
+    
     do {
         curr = zpl_time_rel();
         accum += cast(zpl_u32)curr;
@@ -51,7 +51,7 @@ void zpl_random_init(zpl_random *r) {
     zpl_isize i, j;
     zpl_u32 x = 0;
     r->value = 0;
-
+    
     r->offsets[0] = zpl__get_noise_from_time();
 #ifdef ZPL_MODULE_THREADING
     r->offsets[1] = zpl_atomic32_fetch_add(&zpl__random_shared_counter, 1);
@@ -68,7 +68,7 @@ void zpl_random_init(zpl_random *r) {
     r->offsets[6] = zpl__get_noise_from_time();
     tick = zpl_rdtsc();
     r->offsets[7] = cast(zpl_u32)(tick ^ (tick >> 32));
-
+    
     for (j = 0; j < 4; j++) {
         for (i = 0; i < zpl_count_of(r->offsets); i++) {
             r->offsets[i] = x = zpl__permute_with_offset(x, r->offsets[i]);
@@ -86,7 +86,7 @@ zpl_u32 zpl_random_gen_u32(zpl_random *r) {
             carry = ++r->offsets[i] ? 0 : 1;
         }
     }
-
+    
     r->value = x;
     return x;
 }
@@ -98,7 +98,7 @@ zpl_u32 zpl_random_gen_u32_unique(zpl_random *r) {
     for (i = 0; i < zpl_count_of(r->offsets); i++) {
         x = zpl__permute_with_offset(x, r->offsets[i]);
     }
-
+    
     return x;
 }
 
@@ -117,19 +117,20 @@ zpl_isize zpl_random_gen_isize(zpl_random *r) {
 
 zpl_i64 zpl_random_range_i64(zpl_random *r, zpl_i64 lower_inc, zpl_i64 higher_inc) {
     zpl_u64 u = zpl_random_gen_u64(r);
-    zpl_i64 i = *cast(zpl_i64 *)&u;
     zpl_i64 diff = higher_inc-lower_inc+1;
-    i %= diff;
+    u %= diff;
+    zpl_i64 i;
+    zpl_memcopy(&i, &u, zpl_size_of(u));
     i += lower_inc;
     return i;
 }
 
 zpl_isize zpl_random_range_isize(zpl_random *r, zpl_isize lower_inc, zpl_isize higher_inc) {
     zpl_u64 u = zpl_random_gen_u64(r);
+    zpl_isize diff = higher_inc-lower_inc+1;
+    u %= diff;
     zpl_isize i;
     zpl_memcopy(&i, &u, zpl_size_of(u));
-    zpl_isize diff = higher_inc-lower_inc+1;
-    i %= diff;
     i += lower_inc;
     return i;
 }
@@ -138,10 +139,10 @@ ZPL_ALWAYS_INLINE zpl_f64 zpl__random_copy_sign64(zpl_f64 x, zpl_f64 y) {
     zpl_i64 ix=0, iy=0;
     zpl_memcopy(&ix, &x, zpl_size_of(zpl_i64));
     zpl_memcopy(&iy, &y, zpl_size_of(zpl_i64));
-
+    
     ix &= 0x7fffffffffffffff;
     ix |= iy & 0x8000000000000000;
-
+    
     zpl_f64 r = 0.0;
     zpl_memcopy(&r, &ix, zpl_size_of(zpl_f64));
     return r;
@@ -150,7 +151,7 @@ ZPL_ALWAYS_INLINE zpl_f64 zpl__random_copy_sign64(zpl_f64 x, zpl_f64 y) {
 zpl_f64 zpl_random_range_f64(zpl_random *r, zpl_f64 lower_inc, zpl_f64 higher_inc) {
     zpl_f64 f = cast(zpl_f64)zpl_random_gen_u64(r) / cast(zpl_f64)ZPL_U64_MAX;
     zpl_f64 diff = higher_inc-lower_inc;
-
+    
     f *= diff;
     f += lower_inc;
     return f;
