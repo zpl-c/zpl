3c4bb0ab7f0abda7ad9f44dfdb6867fd2afa6175
Vladyslav Hrytsenko <vladgritsenko@gmail.com>
2022-09-17 16:00:51 +0300

    code: removed old arena code

--- code/source/essentials/memory_custom.c
+++ code/source/essentials/memory_custom.c
@@ -176,12 +176,8 @@ ZPL_ALLOCATOR_PROC(zpl_arena_allocator_proc) {
 
             // NOTE: Out of memory
             if (arena->total_allocated + total_size > cast(zpl_isize) arena->total_size) {
-                if (arena->auto_growth == 0) {
-                    // zpl__printf_err("%s", "Arena out of memory\n");
-                    return NULL;
-                }
-
-
+                // zpl__printf_err("%s", "Arena out of memory\n");
+                return NULL;
             }
 
             ptr = zpl_align_forward(end, alignment);
